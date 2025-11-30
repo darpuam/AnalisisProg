248633aafaea36b23ef18f30bdfcb66165f07937
Davis_0n <Davis@DESKTOP-HCJRUHU>
2025-11-30 03:18:55 +0100

    Add project files.

---
+++ AnalisisProg/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace AnalisisProg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxRegistros = new System.Windows.Forms.GroupBox();
+            this.lstDatos = new System.Windows.Forms.ListBox();
+            this.lblTiempoInicio = new System.Windows.Forms.Label();
+            this.lblTiempoFin = new System.Windows.Forms.Label();
+            this.btnGenerarNumeros = new System.Windows.Forms.Button();
+            this.lblCantidadRegistros = new System.Windows.Forms.Label();
+            this.tbCantidad = new System.Windows.Forms.TextBox();
+            this.lblDuracion = new System.Windows.Forms.Label();
+            this.btnInsertionSort = new System.Windows.Forms.Button();
+            this.btnQuickSort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbBuscar = new System.Windows.Forms.TextBox();
+            this.btnBusquedaSecuencial = new System.Windows.Forms.Button();
+            this.btnBusquedaBinaria = new System.Windows.Forms.Button();
+            this.lstDatos2 = new System.Windows.Forms.ListBox();
+            this.lblDuracion2 = new System.Windows.Forms.Label();
+            this.lblTiempoFin2 = new System.Windows.Forms.Label();
+            this.lblTiempoInicio2 = new System.Windows.Forms.Label();
+            this.groupBoxRegistros.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxRegistros
+            // 
+            this.groupBoxRegistros.Controls.Add(this.lstDatos2);
+            this.groupBoxRegistros.Controls.Add(this.lstDatos);
+            this.groupBoxRegistros.Location = new System.Drawing.Point(12, 268);
+            this.groupBoxRegistros.Name = "groupBoxRegistros";
+            this.groupBoxRegistros.Size = new System.Drawing.Size(349, 225);
+            this.groupBoxRegistros.TabIndex = 0;
+            this.groupBoxRegistros.TabStop = false;
+            this.groupBoxRegistros.Text = "Registros: 0";
+            // 
+            // lstDatos
+            // 
+            this.lstDatos.FormattingEnabled = true;
+            this.lstDatos.ItemHeight = 16;
+            this.lstDatos.Location = new System.Drawing.Point(6, 21);
+            this.lstDatos.Name = "lstDatos";
+            this.lstDatos.Size = new System.Drawing.Size(167, 196);
+            this.lstDatos.TabIndex = 0;
+            // 
+            // lblTiempoInicio
+            // 
+            this.lblTiempoInicio.AutoSize = true;
+            this.lblTiempoInicio.Location = new System.Drawing.Point(9, 517);
+            this.lblTiempoInicio.Name = "lblTiempoInicio";
+            this.lblTiempoInicio.Size = new System.Drawing.Size(161, 16);
+            this.lblTiempoInicio.TabIndex = 1;
+            this.lblTiempoInicio.Text = "Tiempo de inicio: 00:00:00";
+            // 
+            // lblTiempoFin
+            // 
+            this.lblTiempoFin.AutoSize = true;
+            this.lblTiempoFin.Location = new System.Drawing.Point(9, 546);
+            this.lblTiempoFin.Name = "lblTiempoFin";
+            this.lblTiempoFin.Size = new System.Drawing.Size(143, 16);
+            this.lblTiempoFin.TabIndex = 2;
+            this.lblTiempoFin.Text = "Tiempo de fin: 00:00:00";
+            this.lblTiempoFin.Click += new System.EventHandler(this.lblTiempoFin_Click);
+            // 
+            // btnGenerarNumeros
+            // 
+            this.btnGenerarNumeros.Location = new System.Drawing.Point(356, 35);
+            this.btnGenerarNumeros.Name = "btnGenerarNumeros";
+            this.btnGenerarNumeros.Size = new System.Drawing.Size(54, 168);
+            this.btnGenerarNumeros.TabIndex = 3;
+            this.btnGenerarNumeros.Text = "Generar Numeros";
+            this.btnGenerarNumeros.UseVisualStyleBackColor = true;
+            this.btnGenerarNumeros.Click += new System.EventHandler(this.btnGenerarNumeros_Click);
+            // 
+            // lblCantidadRegistros
+            // 
+            this.lblCantidadRegistros.AutoSize = true;
+            this.lblCantidadRegistros.Location = new System.Drawing.Point(11, 24);
+            this.lblCantidadRegistros.Name = "lblCantidadRegistros";
+            this.lblCantidadRegistros.Size = new System.Drawing.Size(141, 16);
+            this.lblCantidadRegistros.TabIndex = 5;
+            this.lblCantidadRegistros.Text = "Cantidad de Registros";
+            // 
+            // tbCantidad
+            // 
+            this.tbCantidad.Location = new System.Drawing.Point(158, 21);
+            this.tbCantidad.Name = "tbCantidad";
+            this.tbCantidad.Size = new System.Drawing.Size(174, 22);
+            this.tbCantidad.TabIndex = 6;
+            // 
+            // lblDuracion
+            // 
+            this.lblDuracion.AutoSize = true;
+            this.lblDuracion.Location = new System.Drawing.Point(9, 571);
+            this.lblDuracion.Name = "lblDuracion";
+            this.lblDuracion.Size = new System.Drawing.Size(139, 16);
+            this.lblDuracion.TabIndex = 7;
+            this.lblDuracion.Text = "Duracion: 0 Segundos";
+            // 
+            // btnInsertionSort
+            // 
+            this.btnInsertionSort.Location = new System.Drawing.Point(173, 76);
+            this.btnInsertionSort.Name = "btnInsertionSort";
+            this.btnInsertionSort.Size = new System.Drawing.Size(146, 52);
+            this.btnInsertionSort.TabIndex = 4;
+            this.btnInsertionSort.Text = "Insertion Sort";
+            this.btnInsertionSort.UseVisualStyleBackColor = true;
+            this.btnInsertionSort.Click += new System.EventHandler(this.btnInsertionSort_Click_1);
+            // 
+            // btnQuickSort
+            // 
+            this.btnQuickSort.Location = new System.Drawing.Point(21, 76);
+            this.btnQuickSort.Name = "btnQuickSort";
+            this.btnQuickSort.Size = new System.Drawing.Size(146, 52);
+            this.btnQuickSort.TabIndex = 8;
+            this.btnQuickSort.Text = "Quick Sort";
+            this.btnQuickSort.UseVisualStyleBackColor = true;
+            this.btnQuickSort.Click += new System.EventHandler(this.btnQuickSort_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Numero a Buscar:";
+            // 
+            // tbBuscar
+            // 
+            this.tbBuscar.Location = new System.Drawing.Point(157, 149);
+            this.tbBuscar.Name = "tbBuscar";
+            this.tbBuscar.Size = new System.Drawing.Size(162, 22);
+            this.tbBuscar.TabIndex = 10;
+            // 
+            // btnBusquedaSecuencial
+            // 
+            this.btnBusquedaSecuencial.Location = new System.Drawing.Point(24, 199);
+            this.btnBusquedaSecuencial.Name = "btnBusquedaSecuencial";
+            this.btnBusquedaSecuencial.Size = new System.Drawing.Size(157, 52);
+            this.btnBusquedaSecuencial.TabIndex = 11;
+            this.btnBusquedaSecuencial.Text = "Busqueda Secuencial";
+            this.btnBusquedaSecuencial.UseVisualStyleBackColor = true;
+            this.btnBusquedaSecuencial.Click += new System.EventHandler(this.btnBusquedaSecuencial_Click_1);
+            // 
+            // btnBusquedaBinaria
+            // 
+            this.btnBusquedaBinaria.Location = new System.Drawing.Point(193, 199);
+            this.btnBusquedaBinaria.Name = "btnBusquedaBinaria";
+            this.btnBusquedaBinaria.Size = new System.Drawing.Size(157, 52);
+            this.btnBusquedaBinaria.TabIndex = 12;
+            this.btnBusquedaBinaria.Text = "Busqueda Binaria";
+            this.btnBusquedaBinaria.UseVisualStyleBackColor = true;
+            this.btnBusquedaBinaria.Click += new System.EventHandler(this.btnBusquedaBinaria_Click_1);
+            // 
+            // lstDatos2
+            // 
+            this.lstDatos2.FormattingEnabled = true;
+            this.lstDatos2.ItemHeight = 16;
+            this.lstDatos2.Location = new System.Drawing.Point(179, 21);
+            this.lstDatos2.Name = "lstDatos2";
+            this.lstDatos2.Size = new System.Drawing.Size(167, 196);
+            this.lstDatos2.TabIndex = 1;
+            // 
+            // lblDuracion2
+            // 
+            this.lblDuracion2.AutoSize = true;
+            this.lblDuracion2.Location = new System.Drawing.Point(190, 571);
+            this.lblDuracion2.Name = "lblDuracion2";
+            this.lblDuracion2.Size = new System.Drawing.Size(139, 16);
+            this.lblDuracion2.TabIndex = 15;
+            this.lblDuracion2.Text = "Duracion: 0 Segundos";
+            // 
+            // lblTiempoFin2
+            // 
+            this.lblTiempoFin2.AutoSize = true;
+            this.lblTiempoFin2.Location = new System.Drawing.Point(190, 546);
+            this.lblTiempoFin2.Name = "lblTiempoFin2";
+            this.lblTiempoFin2.Size = new System.Drawing.Size(143, 16);
+            this.lblTiempoFin2.TabIndex = 14;
+            this.lblTiempoFin2.Text = "Tiempo de fin: 00:00:00";
+            // 
+            // lblTiempoInicio2
+            // 
+            this.lblTiempoInicio2.AutoSize = true;
+            this.lblTiempoInicio2.Location = new System.Drawing.Point(190, 517);
+            this.lblTiempoInicio2.Name = "lblTiempoInicio2";
+            this.lblTiempoInicio2.Size = new System.Drawing.Size(161, 16);
+            this.lblTiempoInicio2.TabIndex = 13;
+            this.lblTiempoInicio2.Text = "Tiempo de inicio: 00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 603);
+            this.Controls.Add(this.lblDuracion2);
+            this.Controls.Add(this.lblTiempoFin2);
+            this.Controls.Add(this.lblTiempoInicio2);
+            this.Controls.Add(this.btnBusquedaBinaria);
+            this.Controls.Add(this.btnBusquedaSecuencial);
+            this.Controls.Add(this.tbBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnQuickSort);
+            this.Controls.Add(this.btnInsertionSort);
+            this.Controls.Add(this.lblDuracion);
+            this.Controls.Add(this.tbCantidad);
+            this.Controls.Add(this.lblCantidadRegistros);
+            this.Controls.Add(this.btnGenerarNumeros);
+            this.Controls.Add(this.lblTiempoFin);
+            this.Controls.Add(this.lblTiempoInicio);
+            this.Controls.Add(this.groupBoxRegistros);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.groupBoxRegistros.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxRegistros;
+        private System.Windows.Forms.ListBox lstDatos;
+        private System.Windows.Forms.Label lblTiempoInicio;
+        private System.Windows.Forms.Label lblTiempoFin;
+        private System.Windows.Forms.Button btnGenerarNumeros;
+        private System.Windows.Forms.Label lblCantidadRegistros;
+        private System.Windows.Forms.TextBox tbCantidad;
+        private System.Windows.Forms.Label lblDuracion;
+        private System.Windows.Forms.Button btnInsertionSort;
+        private System.Windows.Forms.Button btnQuickSort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbBuscar;
+        private System.Windows.Forms.Button btnBusquedaSecuencial;
+        private System.Windows.Forms.Button btnBusquedaBinaria;
+        private System.Windows.Forms.ListBox lstDatos2;
+        private System.Windows.Forms.Label lblDuracion2;
+        private System.Windows.Forms.Label lblTiempoFin2;
+        private System.Windows.Forms.Label lblTiempoInicio2;
+    }
+}
+
---
+++ AnalisisProg/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalisisProg
+{
+
+    public partial class Form1 : Form
+    {
+
+        long[] arregloNumeros;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // --- BOTÓN DE GENERACIÓN DE DATOS ---
+        private void btnGenerarNumeros_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1. Lectura del TextBox renombrado a tbCantidad
+                long cant = long.Parse(tbCantidad.Text);
+
+                // Inicializamos el array con el tamaño fijo exacto
+                arregloNumeros = new long[cant];
+                Random rdn = new Random();
+
+                // Stopwatch es mucho más preciso que DateTime.Now para medir rendimiento
+                Stopwatch sw = new Stopwatch();
+                lblTiempoInicio.Text = "Inicio Gen: " + DateTime.Now.ToString("hh:mm:ss.fff");
+
+                sw.Start();
+                // Bucle optimizado para llenado rápido
+                for (long i = 0; i < cant; i++)
+                {
+                    // Generamos números aleatorios entre 1 y 1 millón
+                    arregloNumeros[i] = rdn.Next(1, 1000000);
+                }
+                sw.Stop();
+
+                lblTiempoFin.Text = "Fin Gen: " + DateTime.Now.ToString("hh:mm:ss.fff");
+                lblDuracion.Text = $"Duración: {sw.ElapsedMilliseconds} ms";
+
+                // PROTECCIÓN DE INTERFAZ:
+                // Intentar renderizar 5,000,000 de filas en un ListBox congelaría la UI.
+                // Solo mostramos los datos si la cantidad es manejable (<= 10,000).
+                if (cant <= 10000)
+                {
+                    lstDatos.DataSource = null;
+                    lstDatos.DataSource = arregloNumeros;
+                }
+                else
+                {
+                    lstDatos.DataSource = null;
+                    MessageBox.Show("Datos generados en memoria. Ocultos por rendimiento.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        // Método recursivo principal de QuickSort
+        private void QuickSort(long[] arr, long low, long high)
+        {
+            if (low < high)
+            {
+                // Obtiene el índice de partición
+                long pi = Partition(arr, low, high);
+
+                // Ordena recursivamente los elementos antes y después de la partición
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
+            }
+        }
+
+        // Lógica de partición (Divide y Vencerás)
+        private long Partition(long[] arr, long low, long high)
+        {
+            long pivot = arr[high]; // Tomamos el último elemento como pivote
+            long i = (low - 1);
+
+            for (long j = low; j < high; j++)
+            {
+                // Si el elemento actual es menor que el pivote
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    // Intercambio (Swap)
+                    long temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            // Intercambio final para colocar el pivote en su lugar correcto
+            long temp1 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp1;
+            return i + 1;
+        }
+
+        // --- QUICK SORT (O(n log n)) ---
+        private async void btnQuickSort_Click_1(object sender, EventArgs e)
+        {
+            if (arregloNumeros == null || arregloNumeros.Length == 0) return;
+
+            btnQuickSort.Enabled = false;
+            lblTiempoInicio.Text = "Iniciando QuickSort...";
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Ejecución en segundo plano
+            await Task.Run(() =>
+            {
+                QuickSort(arregloNumeros, 0, arregloNumeros.Length - 1);
+            });
+
+            sw.Stop();
+            lblTiempoFin.Text = "Fin QuickSort";
+            lblDuracion.Text = $"Duración: {sw.ElapsedMilliseconds} ms";
+            btnQuickSort.Enabled = true;
+
+            if (arregloNumeros.Length <= 10000)
+            {
+                lstDatos.DataSource = null;
+                lstDatos.DataSource = arregloNumeros;
+            }
+        }
+
+        // --- INSERTION SORT (O(n^2)) ---
+        // Usamos 'async' para no bloquear la ventana principal mientras ordena
+        private async void btnInsertionSort_Click_1(object sender, EventArgs e)
+        {
+            if (arregloNumeros == null || arregloNumeros.Length == 0) return;
+
+            btnInsertionSort.Enabled = false; // Desactivar botón para evitar doble clic
+            lblTiempoInicio.Text = "Iniciando Insertion...";
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Task.Run envía el proceso pesado a un hilo secundario del procesador.
+            // Esto permite que la barra de título se pueda mover y no salga "No Responde".
+            await Task.Run(() =>
+            {
+                // Lógica pura de Insertion Sort
+                for (long i = 1; i < arregloNumeros.Length; i++)
+                {
+                    long key = arregloNumeros[i];
+                    long j = i - 1;
+
+                    // Desplaza los elementos mayores hacia la derecha
+                    while (j >= 0 && arregloNumeros[j] > key)
+                    {
+                        arregloNumeros[j + 1] = arregloNumeros[j];
+                        j = j - 1;
+                    }
+                    arregloNumeros[j + 1] = key;
+                }
+            });
+
+            sw.Stop();
+            lblTiempoFin.Text = "Fin Insertion";
+            lblDuracion.Text = $"Duración: {sw.ElapsedMilliseconds} ms";
+            btnInsertionSort.Enabled = true;
+
+            // Actualizar visualización solo si son pocos datos
+            if (arregloNumeros.Length <= 10000)
+            {
+                lstDatos.DataSource = null;
+                lstDatos.DataSource = arregloNumeros;
+            }
+        }
+
+        // --- BÚSQUEDA SECUENCIAL (Lineal - O(n)) ---
+        private void btnBusquedaSecuencial_Click_1(object sender, EventArgs e)
+        {
+            if (arregloNumeros == null) return;
+
+            long objetivo = 0;
+            // Lectura segura del nuevo TextBox tbBuscar
+            if (!long.TryParse(tbBuscar.Text, out objetivo))
+                objetivo = 450; // Valor por defecto si el campo está vacío o es texto
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            bool encontrado = false;
+            // Recorre uno por uno hasta encontrarlo
+            for (int i = 0; i < arregloNumeros.Length; i++)
+            {
+                if (arregloNumeros[i] == objetivo)
+                {
+                    encontrado = true;
+                    break; // Rompe el ciclo apenas lo encuentra para eficiencia
+                }
+            }
+
+            sw.Stop();
+            lblDuracion.Text = $"Secuencial: {sw.Elapsed.TotalMilliseconds} ms. Hallado: {encontrado}";
+        }
+        // --- BÚSQUEDA BINARIA (Logarítmica - O(log n)) ---
+        // REQUISITO: El arreglo DEBE estar ordenado previamente (usar QuickSort primero)
+        private void btnBusquedaBinaria_Click_1(object sender, EventArgs e)
+        {
+            if (arregloNumeros == null) return;
+
+            long objetivo = 0;
+            if (!long.TryParse(tbBuscar.Text, out objetivo)) objetivo = 450;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            long min = 0;
+            long max = arregloNumeros.Length - 1;
+            bool encontrado = false;
+
+            // Algoritmo de división sucesiva
+            while (min <= max)
+            {
+                long mid = (min + max) / 2; // Calcula el punto medio
+                if (objetivo == arregloNumeros[mid])
+                {
+                    encontrado = true;
+                    break;
+                }
+                else if (objetivo < arregloNumeros[mid])
+                {
+                    max = mid - 1; // Descartar mitad derecha
+                }
+                else
+                {
+                    min = mid + 1; // Descartar mitad izquierda
+                }
+            }
+
+            sw.Stop();
+            lblDuracion.Text = $"Binaria: {sw.Elapsed.TotalMilliseconds} ms. Hallado: {encontrado}";
+        }
+
+        private void lblTiempoFin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
